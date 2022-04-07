d1c6e1e6e3c2665cd614200b08df4cef6e86ec0e
AjaykumarGV <ajaykrishnamech@gmail.com>
2022-04-07 19:18:18 +0530

    Tharik Coding is Done

--- src/org/sample/Sample1.java
+++ src/org/sample/Sample1.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Sample1 {
 	
+	private void tc2() {
+		System.out.println("TestCase 2 is Done");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("My Name is Ajay Kumar");
